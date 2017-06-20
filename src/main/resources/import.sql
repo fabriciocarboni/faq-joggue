6aa19001f40569b9395b21b5329ec320e610a6b1
Fabricio Carboni <carboni@gmail.com>
2017-06-19 22:36:43 -0300

    Adding one more line in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 insert into pergunta (id, titulo, resposta) values (1, 'Pergunta1', 'Resposta1');
 insert into pergunta (id, titulo, resposta) values (2, 'Pergunta2', 'Resposta2');
 insert into pergunta (id, titulo, resposta) values (3, 'Pergunta3', 'Resposta3');
+insert into pergunta (id, titulo, resposta) values (3, 'Pergunta4', 'Resposta4');
